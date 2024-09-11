f0c1a2bceb268665043e58be69cb0d7c3316c62d
Mathew Karunaratne <mathewkarunaratne@gmail.com>
2024-09-11 12:39:40 +1000

    Test

---
+++ src/main/java/com/example/addressbook/model/Contact.java
@@ -0,0 +1,69 @@
+package com.example.addressbook.model;
+/**
+ * A simple model class representing a contact with a first name, last name, email, and phone number.
+ */
+public class Contact {
+    /**
+     * Constructs a new Contact with the specified first name, last name, email, and phone number.
+     * @param firstName The first name of the contact
+     * @param lastName The last name of the contact
+     * @param email The email of the contact
+     * @param phone The phone number of the contact
+     */
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Contact(String firstName, String lastName, String email, String phone) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
+}
